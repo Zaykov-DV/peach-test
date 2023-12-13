a410cf3ef38ab5111240be9b8818443be18d7da9
Дмитрий <grunger.ekb@gmail.com>
2023-12-13 17:37:42 +0500

    first commit

--- src/views/CompensationView.vue
+++ src/views/CompensationView.vue
@@ -7,7 +7,7 @@
       <div class="compensation__row">
         <div class="compensation__item" v-for="(item, index) in compensations" :key="index">
           <div class="compensation__icon"
-               :style="{ backgroundImage: `url('../assets/images/icons/${item.icon}.svg')` }"></div>
+               :style="{ backgroundImage: `url('@/assets/images/icons/${item.icon}.svg')` }"></div>
           <span class="compensation__text">{{ item.title }}</span>
         </div>
       </div>
@@ -22,7 +22,7 @@
       <div class="compensation__row">
         <div class="compensation__item" v-for="(item, index) in motivation" :key="index">
           <div class="compensation__icon"
-               :style="{ backgroundImage: `url('../assets/images/icons/${item.icon}.svg')` }"></div>
+               :style="{ backgroundImage: `url('@/assets/images/icons/${item.icon}.svg')` }"></div>
           <span class="compensation__text">{{ item.title }}</span>
         </div>
       </div>
